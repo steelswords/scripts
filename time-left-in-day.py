48c96b4a4956da1f8b3c8b39e572dc0b1ee9ed83
Tristan Andrus <tristan@tristanandrus.com>
2024-05-11 12:51:17 -0600

    Squashed commit of the following:
    commit e7add2c9c8d131b39cbb5e8da61c1a6534c169b6
    Author: Tristan Andrus <tristan@tristanandrus.com>
    Date:   Sat May 11 12:31:26 2024 -0600
    
        Add CLI arguments
    
        * verbose flag
        * list-events flag
        * no-calendar flag
    
        Signed-off-by: Tristan Andrus <tristan@tristanandrus.com>
    
    commit 14a67df86589756d59235289e376d49a7a9bb63b
    Author: Tristan Andrus <tristan@tristanandrus.com>
    Date:   Sat May 11 12:11:13 2024 -0600
    
        Fix time block bug
    
        Signed-off-by: Tristan Andrus <tristan@tristanandrus.com>
    
    commit 1cb00f786e88e46d1800dee06a9009d165748444
    Author: Tristan Andrus <tristan@tristanandrus.com>
    Date:   Sat May 11 12:03:37 2024 -0600
    
        If the local GNOME calendar is synced, all events show up now
    
        Signed-off-by: Tristan Andrus <tristan@tristanandrus.com>
    
    commit c1d0b431919b910ccee3f24c4ce1a1ddd1752235
    Author: Tristan Andrus <tristan@tristanandrus.com>
    Date:   Fri May 10 11:44:02 2024 -0600
    
        WIP: Still having trouble getting events to sync.
    
        Signed-off-by: Tristan Andrus <tristan@tristanandrus.com>
    
    commit aff6e1d22426ad8bf666ea2936b54592cc317879
    Author: Tristan Andrus <tristan@tristanandrus.com>
    Date:   Fri May 10 18:11:05 2024 -0600
    
        Sync sources every time
    
        Signed-off-by: Tristan Andrus <tristan@tristanandrus.com>
    
    commit 07f6490d1d273871a20240ae0c60128dc91ec36a
    Author: Tristan Andrus <tristan@tristanandrus.com>
    Date:   Thu May 9 22:55:06 2024 -0600
    
        Ignore stupid calendars, like Holidays
    
        Signed-off-by: Tristan Andrus <tristan@tristanandrus.com>

--- time-left-in-day.py
+++ time-left-in-day.py
@@ -1,19 +1,53 @@
 #!/usr/bin/python3
 
+import argparse
 import asyncio
 import tzlocal
-from datetime import datetime, timezone, time
+from datetime import datetime, timezone, time, timedelta
 import gi # For calendar integration
 #from ICalGLib import Timezone
 import sys
+from tabulate import tabulate
 
 gi.require_version("EDataServer", "1.2")
 gi.require_version("ECal", "2.0")
 from gi.repository import EDataServer, ECal, ICalGLib
 
+SOURCES_TO_IGNORE = (
+        "Birthdays",
+        "Birthdays & Anniversaries",
+        "Holidays in United States"
+        )
+
+#####################
+# USER CONFIGURATION
+#####################
+class WorkHours:
+    start = (9, 0, 0)
+    end   = (17, 0, 0)
+
+Bedtime = (23, 0, 0)
+
+"""The time in the early morning hours where I assume that I have gone to sleep.
+Before this time, I assume that it's past midnight and I need to go to bed.
+After this time, I assume I have just woken up really early, but I want data for
+hours until work.
+"""
+AssumeAsleepTime = (2, 30, 0)
+
+"""Not really my "scheduled" wake up time, but the time after which I might be woken
+up by offspring and not be able to get back to sleep."""
+ScheduledWakeUpTime = (6, 0, 0)
+
+
 
 def eprint(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
+
+def vprint(*args, **kwargs):
+    """Wrapper around eprint. Only prints when verbose flag is set."""
+    if cliargs.verbose:
+        eprint(*args, **kwargs)
 
 def time_left_until(target_time: datetime) -> str:
     now = datetime.now()
@@ -37,54 +71,66 @@ def main():
 
 async def get_calendar_clients():
     source_registry = EDataServer.SourceRegistry.new_sync(None)
-    print(f"Doing this source registry:")
-    source_registry.debug_dump()
+    if cliargs.verbose:
+        #print(f"Doing this source registry:")
+        #source_registry.debug_dump()
+
+        eprint(f"Doing this source registry (calendars):")
+        source_registry.debug_dump(EDataServer.SOURCE_EXTENSION_CALENDAR)
+
     if not source_registry:
-        print("ERROR: Unable to get calendar events")
+        eprint("ERROR: Unable to get calendar events")
         sys.exit(1)
-    #sources = EDataServer.SourceRegistry.list_sources(source_registry, EDataServer.SOURCE_EXTENSION_CALENDAR)
-    #sources = [s for s in sources if s.has_extension("Calendar")]
-    sources = source_registry.list_enabled("Calendar")
-    sources = [source for source in sources if source.get_display_name() not in ("Birthdays", "Birthdays & Anniversaries", "Holidays in United States")]
+    sources = EDataServer.SourceRegistry.list_sources(source_registry, EDataServer.SOURCE_EXTENSION_CALENDAR)
+    #sources = EDataServer.SourceRegistry.list_sources(source_registry, EDataServer.SOURCE_EXTENSION_GOA)
+    sources = [s for s in sources if s.has_extension(EDataServer.SOURCE_EXTENSION_CALENDAR)]
 
-    print({source.get_display_name(): source for source in sources})
+    # Refresh all the source collections.
+    source_collections = EDataServer.SourceRegistry.list_sources(source_registry, EDataServer.SOURCE_EXTENSION_COLLECTION)
+    for collection in source_collections:
+        vprint("==> Refreshing source collection UID {}: {}".format(collection.get_uid(), collection))
+        source_registry.refresh_backend_sync(collection.get_uid(), None)
 
-    # TODO: Filter obviously bad calendars, like Birthdays and Hllidays
     # TODO: This asynchronously
-
     clients = []
     #print("Sources in order:")
+    num_expected_clients = len(sources)
     for source in sources:
-        client = ECal.Client.connect_sync(source, ECal.ClientSourceType.EVENTS, 15, None)
+        client = ECal.Client.connect_sync(source, ECal.ClientSourceType.EVENTS, 1, None)
         if client:
-            #print(client)
-            print(f"Found source: {source.get_display_name()}")
-            clients.append(client)
+            vprint(f" Client for source {source.get_display_name()}: {client}")
+            source_name = source.get_display_name()
+            if source_name in SOURCES_TO_IGNORE:
+                vprint(f"Ignoring source {source_name}")
+                num_expected_clients = num_expected_clients - 1
+            else:
+                vprint(f"Found source: {source_name}")
+                clients.append(client)
         else:
-            print(f"Err: Could not get client for {source.get_display_name()}")
+            eprint(f"Err: Could not get client for {source.get_display_name()}")
 
-    while len(clients) < len(sources):
+    while len(clients) < num_expected_clients:
         await asyncio.sleep(0.01)
-        #print("waiting for clients to connect...")
 
-    print(f"found {len(sources)} sources and {len(clients)} clients")
+    vprint(f"found {len(sources)} sources and {len(clients)} clients")
     return clients
 
 async def get_calendar_events_between_times(clients: list, start_datetime: datetime, end_datetime: datetime):
     result_events = []
-
-    print(f"get_calendar_events_between_times Passed {len(clients)} clients")
 
     # TODO: Do time zone dynamically
     time_format = "%Y%m%dT%H%M%S"
 
     # Example S-Expression from https://mail.gnome.org/archives/evolution-hackers/2022-March/msg00001.html:
     # (occur-in-time-range? (make-time "20220227T230000Z") (make-time "20220410T000000Z") "Europe/Prague")
-    sexp = f"(occur-in-time-range? (make-time \"{start_datetime.strftime(time_format)}\") (make-time \"{end_datetime.strftime(time_format)}\") \"America/Denver\" )"
-    print(f"******* sexp = {sexp}")
+
+    # occur-in-time-range is an exclusive range, so if something ends at 5,
+    # we have to search for envents ending at 5:00:01
+    one_second = timedelta(seconds=1)
+    sexp = f"(occur-in-time-range? (make-time \"{(start_datetime - one_second).strftime(time_format)}\") (make-time \"{(end_datetime + one_second).strftime(time_format)}\") \"America/Denver\" )"
     for client in clients:
-        print("----------------------")
-        print(f"Checking client {client}")
+        vprint("----------------------")
+        vprint(f"Checking client {client}")
 
         events_from_this_client = []
 
@@ -95,8 +141,7 @@ async def get_calendar_events_between_times(clients: list, start_datetime: datet
                 #if not event.get_dtstart():
                 #    continue
 
-                print(f"))))))Found event {event}")
-                print(f"))))))Summary: {event.get_summary()}, Starts: {event.get_dtstart().get_time()}")
+                vprint(f"** Found event: {event.get_summary()}, Starts: {event.get_dtstart()}")
                 result_events.append(event)
 
         # I have been informed this is deprecated
@@ -118,6 +163,11 @@ def hms_tuple_to_fractional_hours(value: tuple) -> float:
     """Yields the number of hours as a decimal from a tuple of (hours, min, sec)"""
     return value[0] + (value[1] / 60.0) + (value[2] / 3600.0)
 
+def hms_tuple_to_datetime_today(value: tuple) -> datetime:
+    """Takes the (hour, min, sec) tuple and returns a datetime for today at that time."""
+    today = datetime.now()
+    return today.replace(hour=value[0], minute=value[1], second=value[2])
+
 def event_to_time_block_tuple(event) -> list:
     """Transmogrifies an ICal event into a list of the form
     [ (start_hour, start_min, start_sec), (end_hour, end_min, end_sec) ]"""
@@ -132,9 +182,9 @@ def event_to_time_block_tuple(event) -> list:
     start_time = (start_time.hour, start_time.minute, start_time.second)
     end_time = (end_time.hour, end_time.minute, end_time.second)
 
-    #print(f"in event_to_time_block_tuple for event starting at {event.get_dtstart().get_time()} and ending at {event.get_dtend().get_time()}: type= {type(start_time)} start_time = {start_time}, end_time = {end_time}")
+    #vprint(f"in event_to_time_block_tuple for event starting at {event.get_dtstart().get_time()} and ending at {event.get_dtend().get_time()}: type= {type(start_time)} start_time = {start_time}, end_time = {end_time}")
 
-    #print(f"In event_to_time_block_tuple, returning {[start_time, end_time]}")
+    #vprint(f"In event_to_time_block_tuple, returning {[start_time, end_time]}")
     return [start_time, end_time]
 
 def difference_between_hms_tuples(start, later) -> tuple:
@@ -142,7 +192,7 @@ def difference_between_hms_tuples(start, later) -> tuple:
     hours   = later[0] - start[0]
     minutes = later[1] - start[1]
     seconds = later[2] - start[2]
-    print(f"Difference between start={start}, later={later} = ({hours}, {minutes}, {seconds})")
+    #vprint(f"Difference between start={start}, later={later} = ({hours}, {minutes}, {seconds})")
     return (hours, minutes, seconds)
 
 def consolidate_events_into_commitment_blocks(commitments: list):
@@ -169,19 +219,19 @@ def consolidate_events_into_commitment_blocks(commitments: list):
     #    (start_in_localtime, end_in_localtime) = event_to_time_block_tuple(event)
     #    if (0,0,0) == difference_between_hms_tuples(start_in_localtime, end_in_localtime):
     #        event.remove(event)
-    #        eprint("\t-> Skipping due to duration of zero")
+    #        vprint("\t-> Skipping due to duration of zero")
     #        continue
 
 
     for event in commitments:
         (start_in_localtime, end_in_localtime) = event_to_time_block_tuple(event)
 
-        eprint(f"-> Description: '{event.get_summary()}' Begins: {start_in_localtime}, Ends: {end_in_localtime}")
+        vprint(f"-> Description: '{event.get_summary()}' Begins: {start_in_localtime}, Ends: {end_in_localtime}")
         # TODO: Check if we're actually busy during these. Or does that happen upstream?
 
         # Skip all-day events, or events with 0 duration.
         if (0,0,0) == difference_between_hms_tuples(start_in_localtime, end_in_localtime):
-            eprint("\t-> Skipping due to duration of zero")
+            vprint("\t-> Skipping due to duration of zero")
             continue
 
         # If we went through all the blocks and there was no overlap, we need to add
@@ -197,27 +247,26 @@ def consolidate_events_into_commitment_blocks(commitments: list):
 
             # Is it easier to detect not overlapping and avoid that.
             if end_in_localtime < block[0] or start_in_localtime > block[1]:
-                print(f"==> No overlap. Block remains {block}")
+                vprint(f"==> No overlap. Block remains {block}")
                 # No overlap
                 was_case_handled = True
-                was_event_handled = False
             else:
                 # There is overlap.
                 block[0] = min(block[0], start_in_localtime)
                 block[1] = max(block[1], end_in_localtime)
-                print(f"==> Dealt with overlap. Now block = {block}")
+                vprint(f"==> Dealt with overlap. Now block = {block}")
                 was_case_handled = True
                 was_event_handled = True
 
             if not was_case_handled:
-                eprint("ERROR! Unexpected edge case!")
-                eprint(f"start_in_localtime = {start_in_localtime}, end_in_localtime = {end_in_localtime}, committed_blocks = {committed_blocks}, events = {commitments}")
+                vprint("ERROR! Unexpected edge case!")
+                vprint(f"start_in_localtime = {start_in_localtime}, end_in_localtime = {end_in_localtime}, committed_blocks = {committed_blocks}, events = {commitments}")
                 sys.exit(10)
         if not was_event_handled:
             # Add to committed_blocks.
             committed_blocks.append([start_in_localtime, end_in_localtime])
-            eprint("==> This event has no overlap with any blocks. Adding to blocks list.")
-            eprint(f"==> blocks now = {committed_blocks}")
+            #vprint("==> This event has no overlap with any blocks. Adding to blocks list.")
+            vprint(f"==> blocks now = {committed_blocks}")
             
     return committed_blocks
 
@@ -227,46 +276,128 @@ def available_hours_until(commitments: list, start_time: datetime, end_time: dat
     nothing tricky with timezones."""
 
     committed_blocks = consolidate_events_into_commitment_blocks(commitments)
-    print(f"~~~> Committed blocks = {committed_blocks}")
+    vprint(f"~~~> Committed blocks = {committed_blocks}")
     # Iterate over the list and subtract committed blocks from the available number
     # of hours left on the clock.
     clock_hours = (end_time - start_time).total_seconds() / 3600.0
     start_time_tuple = datetime_to_hms_tuple(start_time)
     end_time_tuple = datetime_to_hms_tuple(end_time)
-    print(f"Checking dead time between {start_time_tuple} and {end_time_tuple}")
+    vprint(f"Checking dead time between {start_time_tuple} and {end_time_tuple}")
     for block in committed_blocks:
         # Clamp end of examination time to the start_time and end_time
-        print(f"::Checking fresh block {block}")
+        vprint(f"::Checking fresh block {block}")
+        if block[1] < start_time_tuple:
+            vprint(f"::Skipping block {block} because it ended in the past")
+            continue
+
         if block[0] < start_time_tuple:
             block[0] = start_time_tuple
 
+
         if block[1] > end_time_tuple:
             block[1] = end_time_tuple
-        
-        print(f"::Checking clamped block {block}")
+
+        vprint(f"::Checking clamped block {block}")
         length_of_time_block = difference_between_hms_tuples(block[0], block[1])
         length_of_time_block = hms_tuple_to_fractional_hours(length_of_time_block)
         length_of_time_block = abs(length_of_time_block)
 
-        print(f"Taking off {length_of_time_block} hours because this block coincides with our start time: {block}. Clock hours was {clock_hours}.")
+        vprint(f"Taking off {length_of_time_block} hours because this block coincides with our start time: {block}. Clock hours was {clock_hours}.")
 
         clock_hours = clock_hours - length_of_time_block
-        print(f"Now clock hours is {clock_hours}")
+        vprint(f"Now clock hours is {clock_hours}")
 
     return clock_hours
 
-async def test_main():
-    today = datetime.now()
-    beginning_time = today.replace(hour = 9, minute = 0, second = 0, microsecond = 0)
-    end_time = today.replace(hour = 17, minute = 0, second = 0, microsecond = 0)
+def list_events(events: list):
+    headers = ["Time", "Summary"]
+    data = []
+    for event in events:
+        def dt_to_pretty_string(dt):
+            if not dt:
+                eprint("ERROR: Tried to call dt_to_pretty_string on non-existant dt!")
+                exit(2)
 
-    clients = await get_calendar_clients()
-    events = await get_calendar_events_between_times(clients, beginning_time, end_time)
-    time_left = available_hours_until(events, start_time=beginning_time, end_time=end_time)
+            dt_time = dt.get_time()
+            return time(hour=dt_time[0], minute=dt_time[1], second=dt_time[2]).strftime("%H:%M")
+
+        time_string = f"{dt_to_pretty_string(event.get_dtstart())} - {dt_to_pretty_string(event.get_dtend())}"
+        data.append([
+            time_string,
+            event.get_summary()
+        ])
+    data.sort()
+    table = tabulate(data, headers, tablefmt="grid")
+    print(table)
+
+
+async def test_main():
+    # TODO: Rename `today`
+    today = datetime.now()
+
+    events = [] # Assume nothing going on unless `use_calendar_events` == True
+
+    now_hms_tuple = datetime_to_hms_tuple(today)
+
+    # After waking up, before work starts
+    if AssumeAsleepTime < now_hms_tuple and now_hms_tuple < WorkHours.start:
+        vprint(f"(((((((( Early Morning. Checking time between {now_hms_tuple} and {WorkHours.start}))))))")
+        time_left = difference_between_hms_tuples(now_hms_tuple, WorkHours.start)
+    # During work hours
+    elif WorkHours.start <= now_hms_tuple and now_hms_tuple < WorkHours.end and today.weekday() not in (5,6):
+        vprint(f"(((((((( WORK HOURS. Checking time between {now_hms_tuple} and {WorkHours.end}))))))")
+        beginning_time = hms_tuple_to_datetime_today(WorkHours.start)
+        end_time       = hms_tuple_to_datetime_today(WorkHours.end)
+        if cliargs.use_calendar_events:
+
+            clients = await get_calendar_clients()
+            vprint(f"       Getting events from today between {beginning_time} and {end_time}")
+            events = await get_calendar_events_between_times(clients, beginning_time, end_time)
+        time_left = available_hours_until(events, start_time=datetime.now(), end_time=end_time)
+    # After work, before bed
+    elif WorkHours.end < now_hms_tuple and now_hms_tuple < Bedtime or today.weekday() in (5,6):
+        vprint("(((((((( After Hours, Before Bedtime))))))")
+        if cliargs.use_calendar_events:
+            beginning_time = today
+            end_time = hms_tuple_to_datetime_today(Bedtime)
+
+            clients = await get_calendar_clients()
+            events = await get_calendar_events_between_times(clients, beginning_time, end_time)
+        time_left = available_hours_until(events,
+                                          start_time=datetime.now(),
+                                          end_time=hms_tuple_to_datetime_today(Bedtime))
+
+    # After bedtime
+    elif Bedtime < now_hms_tuple or now_hms_tuple < AssumeAsleepTime:
+        # Calculate amount of time left to sleep
+        wakeup_time = hms_tuple_to_datetime_today(AssumeAsleepTime)
+
+        # If it is still the day before, add that extra time by setting our wakeup
+        # time to tomorrow.
+        if Bedtime < now_hms_tuple:
+            one_day_more = timedelta(days=1)
+            wakeup_time = wakeup_time + one_day_more
+
+        time_left = wakeup_time - datetime.now()
+    else:
+        # Something very unexpected indeed happened
+        time_left = "ERROR"
+
+    if cliargs.use_calendar_events and cliargs.list_events:
+        list_events(events)
     print(time_left)
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+            prog="Time Left in Day",
+            description="Gets the number of hours left in this leg of your day (pre-work, at-work, post-work, post-bedtime",
+            epilog="Not a polished, finished script.")
+    parser.add_argument("-l", "--list-events", action="store_true")
+    parser.add_argument("-v", "--verbose", action="store_true")
+    parser.add_argument("-c", "--no-calendar", action="store_false", default=True, dest="use_calendar_events")
+
+    cliargs = parser.parse_args()
 
     #main()
     asyncio.run(test_main())
